59fd4dcaebf4c0c64d4a61517288cd3f27676d5f
Diego Zapata <zapatad308@gmail.com>
2024-08-29 12:16:09 -0600

    Ejercicios 3 y 4

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,1 +1,118 @@
-'''Ejercicio 3'''
+'''Ejercicio 3
+
+Un hotel de playa cuenta con un recepcionista que se encarga de
+presentar a los clientes las opciones de habitaciones disponibles junto
+con sus precios. Tras la elección de la habitación, el recepcionista
+solicita los datos personales del cliente y el número de noches que
+permanecerá en el hotel. Finalmente, entrega al cliente una factura
+detallada con el total de los gastos.
+
+Adicionalmente, los clientes pueden solicitar servicios extra,
+como el uso de la piscina o la cancha de golf, que tienen un costo
+adicional. Implementa esta funcionalidad en tu programa
+'''
+
+import os
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class extra():
+    def __init__(self,numeroextra):
+        self.numeroextra = numeroextra
+        self.extras = self.definir_Extra()
+    
+    def definir_Extra(self):
+        if self.numeroextra == 0:
+            return "Vacio"
+        elif self.numeroextra == 1:
+            return "Piscina"
+        elif self.numeroextra == 2:
+            return "Cancha de golf"
+
+class habitacion(extra):
+    def __init__(self,numero,fecha_entrada,fecha_salida, numeroextra):
+        super().__init__(numeroextra)
+        self.numero = numero
+        self.tipo = self.definir_tipo()
+        self.costo = self.definir_costo()
+        self.fecha_entrada = fecha_entrada
+        self.fecha_salida = fecha_salida
+    
+    def definir_costo(self):
+        if self.numero == 1:
+            return "$50"
+        elif self.numero == 2:
+            return "$75"
+        elif self.numero == 3:
+            return "$100"
+        else:
+            print("Opcion no valida")
+    
+    def definir_tipo(self):
+        if self.numero == 1:
+            return "Individual"
+        elif self.numero == 2:
+            return "Doble"
+        elif self.numero == 3:
+            return "Cuadruple"
+        else:
+            print("Opcion no valida")
+
+class cliente(habitacion):
+    def __init__(self,nombre, apellido, telefono, dui, nhuespedes, n_noches, numero, fecha_entrada, fecha_salida, numeroextra):
+        super().__init__(numero, fecha_entrada, fecha_salida, numeroextra)
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.dui = dui
+        self.nhuespedes = nhuespedes
+        self.n_noches = n_noches
+        
+class factura(cliente):
+    def __init__(self, nombre, apellido, telefono, dui, nhuespedes, n_noches, numero, fecha_entrada, fecha_salida, numeroextra):
+        super().__init__(nombre, apellido, telefono, dui, nhuespedes, n_noches, numero, fecha_entrada, fecha_salida, numeroextra)
+
+        self.habitacion = [f"Tipo de habitación: {self.tipo}",f"Costo de la habitación: {self.costo}",
+                           f"Fecha de entrada: {fecha_entrada}",f"Fecha de salida: {fecha_salida}",f"Extras: {self.extras}"]
+
+    def generarfactura(self):
+        limpiar_consola()
+        cliente = [f"Nombre: {self.nombre}",f"Apellido: {self.apellido}",f"Telefono: {self.telefono}",
+                   f"Dui: {self.dui}",f"Numero de huespedes: {self.nhuespedes}",f"Numero de noches: {self.n_noches}"]
+        print("================================")
+        for i in range(1,len(cliente)+1):
+            print(f"{cliente[i-1]}")
+        print("================================")
+        for i in range(1,len(self.habitacion)+1):
+            print(f"{self.habitacion[i-1]}")
+        print("================================")
+
+habitacion_huesped = ["1. Habitacion individual $50","2. Habitacion doble $75","3. Habitación cuadruple $100"]
+extras = ["0. Ninguna","1. Piscina $5","2. Cancha de golf $10"]    
+    
+def genfactura():
+    limpiar_consola()
+    for i in range(1,len(habitacion_huesped)+1):
+        print(f"{habitacion_huesped[i-1]}")
+    print("===================================")
+    numero = int(input("Que habitación desea? : "))
+    limpiar_consola()
+    entrada = input("Fecha de entrada: ")
+    salida = input("Fecha de salida: ")
+    limpiar_consola()
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    telefono = input("Telefono: ")
+    dui = input("Dui: ")
+    n_huespedes = int(input("Numero de huespedes: "))
+    noches = int(input("Numero de noches: "))
+    limpiar_consola()
+    for i in range(1,len(extras)+1):
+        print(f"{extras[i-1]}")
+    print("===================================")
+    numeroextra = int(input("Desea algun extra? : "))
+    fac = factura(nombre,apellido,telefono,dui,n_huespedes,noches,numero,entrada,salida,numeroextra)
+    fac.generarfactura()
+
+genfactura()
--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,1 +1,117 @@
-'''Ejercicio 4'''
+'''Ejercicio 4
+
+Una empresa de renta de transporte tiene varios tipos de vehículos a su
+disposición cada uno con sus características y coste de renta. La
+empresa periódicamente registra los nuevos vehículos que ingresan al
+lote para su posterior puesta en renta.
+
+ Implementa la funcionalidad de rentar los vehículos disponibles
+tomando en cuenta los datos del cliente.
+'''
+
+import os
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class NuevoVehiculo():
+    def __init__(self,placa,marca,modelo,año,clase,transmision,color,motor,chasis,precio_renta):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.clase = clase
+        self.asientos = "5.00 asientos"
+        self.ruedas = 4
+        self.transmision = transmision
+        self.color = color
+        self.motor = motor
+        self.chasis = chasis
+        self.precio_renta = precio_renta
+
+    
+    def mostrarinfo(self):
+        tipo = ["Placa: ","Marca: ","Modelo: ","Año: ","Clase: ","Asientos: ",
+                "Ruedas: ","Transmision: ","Color: ","Motor: ","Chasis: ","Precio: $"]
+        lista = [self.placa,self.marca,self.modelo,self.año,self.clase,self.asientos,
+                 self.ruedas,self.transmision,self.color,self.motor,self.chasis,self.precio_renta]
+        print("============ Nuevo Vehiculo ===============")
+        for i in range(1,len(lista)+1):
+            print(f"{tipo[i-1]}{lista[i-1]}")
+
+class Vehiculos():
+    def __init__(self,placa,marca,modelo,año,pais,clase,transmision,color,motor,chasis,precio_renta):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.pais = pais
+        self.clase = clase
+        self.asientos = "5.00 asientos"
+        self.ruedas = 4
+        self.transmision = transmision
+        self.color = color
+        self.motor = motor
+        self.chasis = chasis
+        self.precio_renta = precio_renta
+    
+    def mostrarinfo(self):
+        tipo = ["Placa: ","Marca: ","Modelo: ","Año: ","Pais: ","Clase: ","Asientos: ",
+                "Ruedas: ","Transmision: ","Color: ","Motor: ","Chasis: ","Precio: $"]
+        lista = [self.placa,self.marca,self.modelo,self.año,self.pais,self.clase,self.asientos,
+                 self.ruedas,self.transmision,self.color,self.motor,self.chasis,self.precio_renta]
+        print("============ Vehiculo =================")
+        for i in range(1,len(lista)+1):
+            print(f"{tipo[i-1]}{lista[i-1]}")
+        print("==============================================")
+
+def menu():
+    limpiar_consola()
+    print("========= Menu ========")
+    print("1. Rentar un nuevo carro")
+    print("2. Rentar un carro")
+    print("=======================")
+    opcion = int(input("Que quiere hacer? : "))
+    if opcion == 1:
+        nuevoVehiculo()
+    elif opcion == 2:
+        rentarVehiculo()
+    else:
+        print("Opcion no valida")
+
+def nuevoVehiculo():
+    limpiar_consola()
+    placa = input("Numero de placa: ")
+    marca = input("Marca: ")
+    modelo = input("Modelo: ")
+    año = int(input("Año: "))
+    clase = input("Clase: ")
+    transmision = input("Transmisión: ")
+    color = input("Color")
+    motor = input("Motor: ")
+    chasis = input("Chasis: ")
+    precio = float(input("Rentado por: $"))
+    nuevo = NuevoVehiculo(placa,marca,modelo,año,clase,transmision,color,motor,chasis,precio)
+    limpiar_consola()
+    nuevo.mostrarinfo()
+    print("==============================================")
+
+def rentarVehiculo():
+    limpiar_consola()
+    placa = input("Numero de placa: ")
+    marca = input("Marca: ")
+    modelo = input("Modelo: ")
+    año = int(input("Año: "))
+    pais = input("País: ")
+    clase = input("Clase: ")
+    transmision = input("Transmisión: ")
+    color = input("Color")
+    motor = input("Motor: ")
+    chasis = input("Chasis: ")
+    precio = float(input("Rentado por: $"))
+    rentado = Vehiculos(placa,marca,modelo,año,pais,clase,transmision,color,motor,chasis,precio)
+    limpiar_consola()
+    rentado.mostrarinfo()    
+    print("==============================================")
+
+menu()
